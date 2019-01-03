7d33a20bc9a80bec70540b039de7ba90b559d2b6
mhd <mhd.core.int@gmail.com>
2019-01-03 19:21:22 +0100

    Ajout Main.js

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+  function poporn () {console.log(alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please'))}
+  setTimeout (poporn, 10000)
+
+  function psyMode() {
+    document.body.style.background = "magenta";
+  }
